8f2f91e3aae8e73fa6a8c83e1a7a8ed30882c677
ndiezcoppel <ndiez@coppel.com.ar>
2025-02-12 16:33:28 -0300

    modal animation

--- react/shared/components/modal/Modal.tsx
+++ react/shared/components/modal/Modal.tsx
@@ -1,6 +1,6 @@
 //@ts-ignore
 import { SvgIcon } from 'coppelar.components/index'
-import React from 'react'
+import React, { useState } from 'react'
 import modal from './modal.module.css'
 
 interface ModalProps {
@@ -11,11 +11,21 @@ interface ModalProps {
 }
 
 export const Modal: React.FC<ModalProps> = ({ handleShow, children }) => {
+  const [closing, setClosing] = useState(false)
+
+  const closeModal = () => {
+    setClosing(true)
+    setTimeout(() => {
+      handleShow()
+      setClosing(false)
+    }, 300)
+  }
+
   return (
     <>
       <div className={modal['cp-modal-background']}>
-        <div className={modal['cp-modal']}>
-          <SvgIcon name="close" stylesProps={{ zIndex: '999', width: '1.5rem', cursor: 'pointer', right: '1.5rem', position: 'absolute' }} onClick={handleShow} />
+        <div className={`${modal['cp-modal']} ${closing ? modal['closing'] : ''}`}>
+          <SvgIcon name="close" stylesProps={{ zIndex: '999', width: '1.5rem', cursor: 'pointer', right: '1.5rem', position: 'absolute' }} onClick={closeModal} />
           {children}
         </div>
       </div>
